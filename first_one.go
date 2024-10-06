05e102876de86467a14b36e29a97b76455924237
Maddini Madhu Chowdary <madhumaddini@gmail.com>
2024-10-06 18:57:45 +0530

    assigning variable to a function
    assigning variable to a function2

--- first_one.go
+++ first_one.go
@@ -1,34 +1,25 @@
+// Assigning a function to a variable
 package main
 
-type celsius float32
-type fahrenheit float32
-type kelvin float32
+import (
+	"fmt"
+	"math/rand"
+)
 
-func (k kelvin) celsius() celsius {
-	return celsius(k - 273.15)
+type kelvin float64
+
+func fakeSensor() kelvin {
+	return kelvin(rand.Intn(151) + 150)
 }
 
-func (k kelvin) fahrenheit() fahrenheit {
-	temp := k.celsius()
-	return temp.fahrenheit()
+func realSensor() kelvin {
+	return 0
 }
 
-func (c celsius) fahrenheit() fahrenheit {
-	return fahrenheit((c * 9.0)/5.0 + 32.0)
-}
-
-func (c celsius) kelvin() kelvin {
-	return kelvin(c + 273.15)
-}
-
-func (f fahrenheit) celsius() celsius {
-	return celsius(5.0 * (f - 32.0) / 9.0)
-}
-
-func (f fahrenheit) kelvin() kelvin {
-	temp := f.celsius()
-	return temp.kelvin()
-}
 func main() {
-    
+	var sensor func() kelvin = fakeSensor
+	fmt.Println(sensor())
+
+	sensor = realSensor
+	fmt.Println(sensor())
 }
