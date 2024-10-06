4036c0c37f6a541f7a2b49c9d763ee2abe15f795
Maddini Madhu Chowdary <madhumaddini@gmail.com>
2024-10-06 20:29:06 +0530

    function types, anonymous functions, closure

--- first_one.go
+++ first_one.go
@@ -8,6 +8,7 @@ import (
 )
 
 type kelvin float64
+type sensor func() kelvin
 
 func fakeSensor() kelvin {
 	return kelvin(rand.Intn(151) + 150)
@@ -17,15 +18,41 @@ func realSensor() kelvin {
 	return 0
 }
 
-func measureTemperature(samples int, sensor func() kelvin) {
+func measureTemperature(samples int, sensor sensor) {
 	for i := 0; i < samples; i++ {
 		fmt.Println(sensor())
 		time.Sleep(time.Second)
 	}
+}	
+
+// anonymous functions or function literals
+var min = func(a, b int) int {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func calibrate(s sensor, offset kelvin) sensor {
+	return func() kelvin {
+		return s() + offset
+	}
 }
 
 func main() {
-	measureTemperature(3, fakeSensor)
-	fmt.Println("-----------------")
-	measureTemperature(3, realSensor)
+	rfunc := calibrate(realSensor, 5)
+	// rfunc keeps reference to the realSensor function and the offset value
+	measureTemperature(3, rfunc)
+
+	fmt.Println("-----------")
+
+	var k kelvin = 294
+	sensor := func() kelvin {
+		return k
+	}
+
+	fmt.Println(sensor())
+	k++
+	fmt.Println(sensor()) // sensor keeps reference to the k variable
 }
