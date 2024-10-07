150e300c24db422a0d2b074cf16a7c716d5be963
Maddini Madhu Chowdary <madhumaddini@gmail.com>
2024-10-08 00:02:31 +0530

    collections : slices append, len cap, make, variadic functions

--- first_one.go
+++ first_one.go
@@ -1,50 +1,61 @@
-/*
- Experiment: terraform.go
- Write a program to terraform a slice of strings by prepending each planet with "New ".
-Use your program to terraform Mars, Uranus, and Neptune.
- Your first iteration can use a terraform function, but your final implementation should
-introduce a Planets type with a terraform method, similar to sort.StringSlice.
-*/
-
 package main
 
 import "fmt"
 
-type Planets []string
-
-func (p Planets) terraform() {
-	for i := range p {
-		switch p[i] {
-		case "Mars", "Uranus", "Neptune":
-			p[i] = "New " + p[i]
-		}
-	}
+func dump(label string, slice []string) {
+	fmt.Printf("%-15v length : %v | capacity : %v | %v\n", label, len(slice), cap(slice), slice)
 }
 
-func terraform(planets []string) {
-	for i := range planets {
-		switch planets[i] {
-		case "Mars", "Uranus", "Neptune":
-			planets[i] = "New " + planets[i]
-		}
+func terraform(prefix string, planets ...string) []string {
+	// variadic function receive the parameters as slices 
+
+	newplanets := make([]string, len(planets))
+	// making a copy, if `planets` were from a slice then editing `planets` can change that slice
+
+	for i := range newplanets {
+		newplanets[i] = prefix + " " + planets[i]
 	}
+	return newplanets
 }
 
 func main() {
-	planets := []string{
-		"Mercury",
-		"Venus",
-		"Earth",
-		"Mars",
-		"Jupiter",
-		"Saturn",
-		"Uranus",
-		"Neptune",
+	numbers := []string{
+		"one",
+		"two",
+		"three",
 	}
 
-	fmt.Println(planets)
-	// terraform(planets)
-	Planets(planets).terraform()
-	fmt.Println(planets)
+	// length, capacity
+	dump("numbers", numbers)
+	numbers = append(numbers, "four", "five")
+	dump("numbers+", numbers) // double the capacity
+	dump("numbers[1:3]", numbers[1:3])
 
+	nums1 := []int{1, 2, 3, 4, 5}
+	nums2 := append(nums1, 6)
+	nums3 := append(nums2, 7, 8, 9)
+
+	nums3[3] = 99 // doesn't affect nums1
+	fmt.Printf("%v\n%v\n%v\n", nums1, nums2, nums3)
+
+	arr1 := []int{1, 2, 3}
+	arr2 := arr1[:1:3] // check this ;)
+	arr2 = append(arr2, 4)
+	fmt.Printf("%v\n%v\n", arr1, arr2)
+	// depending on capacity of `arr2` `arr1` could be overwritten
+
+	// Preallocating some length and capacity using `make`
+	array := make([]int, 0, 10)
+	array = append(array, 1, 2, 3, 4)
+	fmt.Println("array: ", array)
+
+	// variadic functions
+	fmt.Println(terraform("New", "Venus", "Mars"))
+	
+	// passing slice in a variadic function
+	planets := []string {
+		"Venus",
+		"Mars",
+	}
+	fmt.Println(terraform("Newer", planets...))
 }
