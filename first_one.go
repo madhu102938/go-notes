497ca928a52f28bd31cdd223a6177548c524f2e1
Maddini Madhu Chowdary <madhumaddini@gmail.com>
2024-10-07 18:42:12 +0530

    collections slices basics

--- first_one.go
+++ first_one.go
@@ -4,29 +4,8 @@ import (
 	"fmt"
 )
 
-func random(a [5]string) {
-	for i, j := range a {
-		fmt.Println(i, j)
-	}
-}
-
 func main() {
-	var planets [8]string
-	planets[0] = "mercury"
-	planets[1] = "venus"
-	planets[2] = "earth"
-
-	fmt.Println(planets[2])
-	fmt.Println(len(planets))
-	fmt.Println(planets[3] == "") // initialized with zero values
-
-	// composite literals
-	names := [5]string{"madhu", "somename", "othername", "even", "odd"}
-	for i := 0; i < len(names); i++ {
-		// fmt.Println(names[i])
-	}
-
-	planets2 := [...]string{ 
+	planets := [...]string{
 		"Mercury",
 		"Venus",
 		"Earth",
@@ -34,27 +13,37 @@ func main() {
 		"Jupiter",
 		"Saturn",
 		"Uranus",
-		"Neptune",// last comma is required
-	} 
-	fmt.Println(len(planets2))
+		"Neptune",
+	}
+
+	terra := planets[:4]
+	gasGaint := planets[4:6]
+	iceGaint := planets[6:]
+	myPlanet := terra[2:3]
+
 	
-	random(names, // comma required
-	)
+	fmt.Println(planets)
+	fmt.Println(terra, gasGaint, iceGaint)
+	fmt.Println(myPlanet)
 
-	// random(planets) // error, size doesn't match
+	myPlanet[0] = "Blue-chan"
 
-	// 2d arrays
-	var nums [2][2]int
-	for i := range nums[0] {
-		nums[0][i] = 5
-	}
+	fmt.Println(planets)
+	fmt.Println(terra, gasGaint, iceGaint)
+	fmt.Println(myPlanet)
 
-	for i := range nums {
-		for j := range nums[i] {
-			fmt.Print(nums[i][j], " ")
-		}
-		fmt.Println()
-	}
+	parentString := "full string"
+	subString := parentString[5:]
 
+	fmt.Println(parentString,",", subString)
 
+	subString = "another string"
+	fmt.Println(parentString,",", subString)
+
+	fmt.Printf("this of type %T\n", planets) // array
+	fmt.Printf("this of type %T\n", gasGaint) // slice
+
+	// initializing a slice
+	names := []string{"Ceres", "Pluto", "Haumea", "Makemake", "Eris"}
+	fmt.Printf("%T, %T\n", names, planets)
 }
