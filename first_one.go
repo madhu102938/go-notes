d104488d68b2be7a7ea693bdd5b287431bb4eb92
Maddini Madhu Chowdary <madhumaddini@gmail.com>
2024-10-11 11:34:33 +0530

    Interface embedding and struct embedding

--- first_one.go
+++ first_one.go
@@ -1,83 +1,65 @@
-// Interfaces
+// interfaces
+// interface embedding and struct embedding
 
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"strings"
 )
 
-type martial struct {}
-type laser int
-
-func (l laser)	talk() string {
-	return strings.Repeat("pew ", int(l))
+type first interface {
+	printString()
 }
 
-func (m martial) talk() string {
-	return "nack nack"
+type second interface {
+	returnInt() int
 }
 
-// types for reusability
-type talker interface {
-	talk() string
+// interface embedding
+type final interface {
+	first
+	second
 }
 
-// an interface is satisfied if the type implements the necessary methods
-// else we will get compile time errors
-func shout(t talker) { 
-	fmt.Println(strings.ToUpper(t.talk()))
+type struct1 struct {}
+
+func (s struct1) printString() {
+	fmt.Println("printing from PrintString")
 }
 
-// Stringer interface, need to implement the String() method
-func (m martial) String() string {
-	return fmt.Sprintf("nack nack from stringer interface")
+type struct2 struct {}
+
+func (s struct2) returnInt() int {
+	return 2
 }
 
-// Marshaler interface, need to implement the MarshalJSON() method
-// func (m martial) MarshalJSON() ([]byte, error)
-func (m martial) MarshalJSON() ([]byte, error) {
-	str := `"knock knock"` // if not enclosed in `` then getting error T_T
-	ans := []byte(str)
-	fmt.Println(ans)
-	return ans, nil
+// struct embedding
+type finalStruct struct {
+	struct1
+	struct2
 }
 
 func main() {
-	var t interface {
-		talk() string
-	}
+	var i1 first
+	var i2 second
+	var i3 final
+	var s1 struct1
+	var s2 struct2
+	var s3 finalStruct
 
-	t = martial{}
-	fmt.Println(t.talk())
-	shout(t)
+	i1 = s1
+	i2 = s2
+	i3 = s3
+
+	i1.printString()
+	fmt.Println(i2.returnInt())
 	
-	t = laser(2)
-	fmt.Println(t.talk())
-	shout(t)
-	// Polymorphism achieved :)
-
-	// Using interfaces with struct embedding
-	var starship struct{ laser }
-	starship.laser = 3 
-	//  Now the starship also satisfies the talker interface, 
-	// allowing it to be used with shout
-	shout(starship)
-
-	// Any code can implement interfaces, even code that already exists (check obsidian)
-
-	// Interfaces provided by the standard library
-	// io.Reader, io.Writer, fmt.Stringer, json.Marshaler, json.Unmarshaler
-	// fmt.Stringer is used to print the value of a type
-	// json.Marshaler is used to convert a type to JSON
-	// json.Unmarshaler is used to convert JSON to a type
-	fmt.Println(martial{})
-
-	bytes, err := json.Marshal(martial{})
-	if err == nil {
-		fmt.Println(string(bytes))
-	} else {
-		fmt.Println(err)
-	}
+	i3.printString()
+	fmt.Println(i3.returnInt())
 }
+
+/*
+what is interface embedding?
+- interface embedding is a way to compose interfaces by including other interfaces as part of the definition of a new interface.
+only the type which satisfies all the embedded interfaces is said to satisfy the final interface.
+*/
