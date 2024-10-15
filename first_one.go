e8deda6c21cac6c7bef1018485cca800c1b91bed
Maddini Madhu Chowdary <madhumaddini@gmail.com>
2024-10-15 14:49:47 +0530

    nil

--- first_one.go
+++ first_one.go
@@ -1,43 +1,94 @@
-// Interfaces and Pointers
+// nil
+// A pointer with nowhere to point has the value nil.
+// And the nil identifier is the zero value for slices, maps, and interfaces too.
 
 package main
 
 import (
 	"fmt"
-	"strings"
+	"sort"
+	// "log"
 )
 
-type talker interface {
-	talk() string
+type person struct {
+	age int
 }
 
-type martian struct {}
-
-func (m martian) talk() string {
-	return "Pew Pew"
+func (p *person) birthday() {
+	if p == nil {
+		// log.Fatal("nil pointer dereference") // is eqivalent to print followed by exit
+		fmt.Println("nil pointer dereference")
+		return
+	}
+	p.age++ // dereferencing a nil pointer will cause a panic
 }
 
-type celsius float64
-
-func (c *celsius) talk() string {
-	return "It's Hot"
-}
-
-func shout(t talker) {
-	fmt.Println(strings.ToUpper(t.talk()))
+func sortStrings(s []string, less func(i, j int) bool) {
+	if less == nil {
+		less = func(i, j int) bool {
+			return s[i] > s[j]
+		}
+	}
+	sort.Slice(s, less)
 }
 
 func main() {
-	shout(martian{})
-	shout(&martian{})
-	/*
-	Interface is satisfied by both martian and *martian
-	*/
+	var nobody *person
+	fmt.Printf("%T %[1]v\n", nobody)
+	nobody.birthday()
 
-	first := celsius(1)
-	shout(first)	// gives error T_T (should match the exact method definition)
-	shout(&first)
-	/*
-	Interface is satisfied by *celsius but not celsius (as the method is defined for *celsius)
-	*/
+	// nil function values
+	// zero value for a function type is nil
+	var f func(a, b int) int
+	fmt.Printf("%T %[1]v\n", f)
+	// f(2, 3) // panic: call of nil function
+
+	food := []string{"onion", "carrot", "celery"}
+	sortStrings(food, nil)
+	fmt.Println(food)
+
+	less := func(i, j int) bool {
+		return len(food[i]) > len(food[j])
+	}
+
+	sortStrings(food, less)
+	fmt.Println(food)
+
+	// nil slices
+	// var first []string // nil slice
+	// var second []string = make([]string, 0) // empty slice
+	// var second = []string{} // empty slice
+
+	var soup []string
+	fmt.Println(soup == nil) // Prints true
+
+	for _, ingredient := range soup {
+		fmt.Println(ingredient)
+	}
+	
+	fmt.Println(len(soup), cap(soup)) // Prints 0 0
+	
+	soup = append(soup, "onion", "carrot", "celery")
+	fmt.Println(soup) // [onion carrot celery]
+	// Range, len, cap and append work with nil slices
+
+	// nil maps
+	var m map[string]int // nil map
+	// var m2 = make(map[string]int) // empty map
+
+	fmt.Println(m == nil) // Prints true
+	
+	if value, ok := m["first"]; ok {
+		fmt.Println(value)
+	}
+	// Writing to a nil map (soup["onion"] = 1) will panic with: assignment to entry in nil map
+
+	// nil interfaces
+	var v interface{}
+	fmt.Println(v == nil)
+
+	var p *person
+	v = p
+	fmt.Printf("%T %[1]v %v\n", v, v == nil) // *main.person <nil> false
+	fmt.Printf("%#v\n", v) // (*main.person)(nil)
 }
