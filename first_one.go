2fee095366ddbc36bd3f8732be2329c38fc9b47a
Maddini Madhu Chowdary <madhumaddini@gmail.com>
2024-10-18 23:48:54 +0530

    Concurrency - Go routines, Channels

--- first_one.go
+++ first_one.go
@@ -1,20 +1,36 @@
-// Exercise : to err is human
+// concurrency
+// go routines, channels
 
 package main
 
 import (
 	"fmt"
-	"net/url"
+	"time"
 )
 
 func main() {
-	google, err := url.Parse("https://a b.com/")
-	if err != nil {
-		if errs, ok := err.(*url.Error); ok {
-			fmt.Printf("%#v\n", errs)
-			// &url.Error{Op:"parse", URL:"https://a b.com/", Err:" "}
-		}
-		return
+	for i := 0; i < 5; i++ {
+		go sleepyGopher(i) // goroutines don't run in order
 	}
-	fmt.Println(google)
+	time.Sleep(1 * time.Second) 
+	// without this line, the program will exit before the goroutines finish
+
+
+	// channels
+	c := make(chan int)
+	for i := 0; i < 5; i++ {
+		go sleepyGopher2(i, c)
+	}
+	for i := 0; i < 5; i++ {
+		fmt.Println(<- c) // it will wait until some is written
+	}
+}
+
+// Could be some function with a long running time.
+func sleepyGopher(id int) {
+	fmt.Println(id, "...snore...")
+}
+
+func sleepyGopher2(id int, c chan int) {
+	c <- id // it will wait until it is read
 }
