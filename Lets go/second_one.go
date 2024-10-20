94f529c9ff01d85803b85115f249631531b9dca9
Maddini Madhu Chowdary <madhumaddini@gmail.com>
2024-10-20 11:18:25 +0530

    started web programming with go :)

---
+++ Lets go/second_one.go
@@ -0,0 +1,45 @@
+// Starting from the book "Lets go"
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// creating a new server mux which means a new router
+	mux := http.NewServeMux()
+
+	// subtree path
+	mux.HandleFunc("/", MainHandler)
+	// fixed path
+	mux.HandleFunc("/snippet", SnippetHandler)
+	// fixed path
+	mux.HandleFunc("/snippet/create", CreateHandler)
+	
+	fmt.Println("Server started on 8080")
+	
+	// by default, localhost is used
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
+
+func CreateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("create a new snippet"))
+}
+
+func SnippetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a snippet"))
+}
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	// if the path is not the root path, then return a 404
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Display the home page"))
+}
+
--- first_one.go
+++ first_one.go
@@ -1,29 +1,59 @@
-// stacks, queues
-// 1. Run `go get github.com/golang-collections/collections`
-// Then run the code below
+// webprogrammming, sending http requests
 
 package main
 
 import (
 	"fmt"
-	"github.com/golang-collections/collections/stack"
-	"github.com/golang-collections/collections/queue"
+	"html/template"
+	"log"
+	"net/http"
 )
 
-func main() {
-	s := stack.New()
-	s.Push(1)
-	s.Push(2)
-	s.Push(3)
-	fmt.Println(s.Pop())
-	fmt.Println(s.Pop())
-	fmt.Println(s.Pop())
+// you can access the documentation with `go doc FunctionName`
 
-	q := queue.New()
-	q.Enqueue(1)
-	q.Enqueue(2)
-	q.Enqueue(3)
-	fmt.Println(q.Dequeue())
-	fmt.Println(q.Dequeue())
-	fmt.Println(q.Dequeue())
+var templates *template.Template
+var homeTemplate *template.Template
+// using a global variable for templates, so that it can be accessed by all handlers
+
+func main() {
+	PopularTemplates()
+
+	http.HandleFunc("/profile", ProfileHandler)
+	http.HandleFunc("/profile/", ProfileHandler)
+	// if you don't add '/' at the end, then it will not match the url
+
+	http.HandleFunc("/", HomeHandler)
+	// if nothing is matched, then '/' is used, thus it needs to at bottom
+
+	fmt.Println("server started on 8081")
+	http.ListenAndServe("0.0.0.0:8081", nil)
+	// nil is used because we are using default server mux
+}
+
+// PopularTemplates parses all the templates in the directory.
+func PopularTemplates() {
+	var err error
+	templates, err = template.ParseGlob("D:/web_develop/Simon_game/*.html")
+	// goes to the directory and parses all the html files
+
+	if err != nil {
+		log.Fatal("templates couldn't be read", err)
+	}
+
+	homeTemplate = templates.Lookup("index.html")
+	// this is used to get the template from the templates
+}
+
+// ProfileHandler handles "/profile" page.
+func ProfileHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "me developer, me go lang, plz hire me :)")
+}
+
+// HomeHandler handles "/" page.
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprint(w, "hehe starting page, about time")
+	if r.Method == "GET" {
+		homeTemplate.Execute(w, nil)
+		// applies the template to the response writer
+	}
 }
