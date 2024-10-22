fd66d8b28b69ffbe9776937a60ba05c2b54545d3
Maddini Madhu Chowdary <madhumaddini@gmail.com>
2024-10-22 14:59:24 +0530

    added commandline arguments

--- Lets go/snippetbox/cmd/web/main.go
+++ Lets go/snippetbox/cmd/web/main.go
@@ -3,12 +3,23 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":8080", "HTTP network address")
+	/* we can also use
+	var addr *string = new(string)  // new(Type) returns pointer to Type, allocates memory
+	flag.StringVar(addr, "addr", ":8080", "HTTP network address")
+	*/
+
+	//  You need to call this *before* you use the addr variable
+    // otherwise it will always contain the default value of ":4000". If any errors are
+    // encountered during parsing the application will be terminated
+	flag.Parse()
+
 	// creating a new server mux which means a new router
 	mux := http.NewServeMux()
 
@@ -24,10 +35,11 @@ func main() {
 
 	mux.HandleFunc("/download/", DownloadHandler)
 	
-	fmt.Println("Server started on 8080")
+	// `addr` is pointer to string, thus we need to dereference it
+	log.Println("Server started on", *addr)
 	
 	// by default, localhost is used
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
 
